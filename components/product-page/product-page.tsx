a380924604386047b4cf602f26a31aafb517fa77
Marcus Vinícius <marcusvinici25@gmail.com>
2024-01-12 08:31:05 -0300

    adding new image to product page

--- components/product-page/product-page.tsx
+++ components/product-page/product-page.tsx
@@ -5,13 +5,26 @@ import { useCartContext } from "@/contexts/use-cart-context";
 
 type ProductPageProps = {
   data: {
-    sync_product: { name: string, thumbnail_url: string, currency: string, description: string },
-    sync_variants: { availability_status: string; size: string; retail_price: string, image: string, product: {image: string} }[]
-  }
+    sync_product: {
+      name: string;
+      thumbnail_url: string;
+      currency: string;
+      description: string;
+    };
+    sync_variants: {
+      availability_status: string;
+      size: string;
+      retail_price: string;
+      image: string;
+      product: { image: string };
+    }[];
+  };
 };
 
 export default function ProductPage({ data }: ProductPageProps) {
-  const [sizeIndex, setSizeIndex] = useState<number>(data?.sync_variants?.length);
+  const [sizeIndex, setSizeIndex] = useState<number>(
+    data?.sync_variants?.length
+  );
   const { back } = useRouter();
   const context = useCartContext();
   console.log("sizeIndex", data);
@@ -27,10 +40,20 @@ export default function ProductPage({ data }: ProductPageProps) {
       </div>
       <div className={styles.product}>
         <div className={styles.productImage}>
-          <img src={data?.sync_product?.thumbnail_url}
-               alt={data?.sync_product?.name}
-               width={619.9}
-               height={619.9}/>
+          <img
+            src={data?.sync_product?.thumbnail_url}
+            alt={data?.sync_product?.name}
+            width={619.9}
+            height={619.9}
+          />
+          {data?.sync_variants?.length > 0 && (
+            <img
+              src={data?.sync_variants[sizeIndex - 1]?.product?.image}
+              alt={data?.sync_product?.name}
+              width={619.9}
+              height={619.9}
+            />
+          )}
         </div>
         <div className={styles.productDescription}>
           <div>
@@ -38,21 +61,28 @@ export default function ProductPage({ data }: ProductPageProps) {
           </div>
           <div className={styles.money}>
             <span>
-              {sizeIndex > 0 && data?.sync_variants[sizeIndex - 1]?.retail_price}{" "}
+              {sizeIndex > 0 &&
+                data?.sync_variants[sizeIndex - 1]?.retail_price}{" "}
               {data?.sync_product?.currency}
             </span>
           </div>
 
-          <div style={{
-            display: "flex",
-            padding: "1.5rem 1rem 1.5rem 0",
-            flexWrap: "wrap",
-          }}>
+          <div
+            style={{
+              display: "flex",
+              padding: "1.5rem 1rem 1.5rem 0",
+              flexWrap: "wrap",
+            }}
+          >
             {data?.sync_variants?.map((item: any, key: number) => (
-              <button key={key}
-                      disabled={item.availability_status !== "active"}
-                      onClick={() => setSizeIndex(key + 1)}
-                      className={`${styles.cardSize} ${!isOpacity(key) ? styles.activeSize : ""}`}>
+              <button
+                key={key}
+                disabled={item.availability_status !== "active"}
+                onClick={() => setSizeIndex(key + 1)}
+                className={`${styles.cardSize} ${
+                  !isOpacity(key) ? styles.activeSize : ""
+                }`}
+              >
                 <h2>{item.size}</h2>
               </button>
             ))}
@@ -64,9 +94,16 @@ export default function ProductPage({ data }: ProductPageProps) {
               justifyContent: "center",
             }}
           >
-            <button className={styles.button}
-                    disabled={data.sync_variants[sizeIndex - 1]?.availability_status !== "active"}
-                    onClick={() => {context?.setCarts([...context?.carts, {...data, sizeIndex}])}}>
+            <button
+              className={styles.button}
+              disabled={
+                data.sync_variants[sizeIndex - 1]?.availability_status !==
+                "active"
+              }
+              onClick={() => {
+                context?.setCarts([...context?.carts, { ...data, sizeIndex }]);
+              }}
+            >
               Add to Cart
             </button>
           </div>
